59e10b922a122f9ae915c73ffdc2005778485438
Jason Tokarz <jrt@binarycows.com>
2019-09-09 19:48:28 +0100

    Update test application to use QObject 'registration' functionality

--- main.cpp
+++ main.cpp
@@ -4,18 +4,28 @@
 #include <QtGui/QApplication>
 #include <QtGui/QDialog>
 #include <QtGui/QLayout>
+#include <QtGui/QSlider>
 #include "QStackedBar.hpp"
 
 int main(int argc, char* argv[])
 {
     QApplication app(argc, argv);
-    QStackedBar* stackedBar = new QStackedBar(nullptr, 4);
+    QStackedBar* stackedBar = new QStackedBar(4);
     QDialog* dialog = new QDialog();
     QVBoxLayout* layout = new QVBoxLayout();
-    QTimer* timer = new QTimer();
+    std::vector<QSlider*> sliders{};
+
 
     dialog->setLayout(layout);
     layout->addWidget(stackedBar);
+
+    for (auto i=0; i<4; i++ )
+    {
+        sliders.push_back(new QSlider(Qt::Horizontal));
+        stackedBar->mapToSegment(sliders[i], i);
+        QObject::connect(sliders[i], SIGNAL(valueChanged(int)), stackedBar, SLOT(setValue(int)));
+        layout->addWidget(sliders[i]);
+    }
 
     stackedBar->setValues({10, 20, 30, 40});
     stackedBar->setMaximumHeight(20);
@@ -26,7 +36,7 @@ int main(int argc, char* argv[])
     stackedBar->setColor(2, QColor::fromRgb(0, 0, 255));
     stackedBar->setColor(3, QColor::fromRgb(255, 255, 0));
 
-    dialog->setFixedSize(500, 100);
+    dialog->setFixedSize(500, 200);
     dialog->show();
 
     return app.exec();
